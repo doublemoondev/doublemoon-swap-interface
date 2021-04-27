29d89179cecf99dd092a11caece00f19bf7f42ee
DoubleMoon Dev <doublemmon.dev01@gmail.com>
2021-04-27 16:27:41 +0700

    Update icon and title

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -48,7 +48,7 @@ const Marginer = styled.div`
   margin-top: 5rem;
 `
 
-const CACHE_KEY = 'pancakeSwapLanguage'
+const CACHE_KEY = 'doubleMoonSwapLanguage'
 
 export default function App() {
   const [selectedLanguage, setSelectedLanguage] = useState<any>(undefined)
