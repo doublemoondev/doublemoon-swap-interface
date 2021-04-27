f82e301b6dcda378ee71672b80ec464df6e09be9
DoubleMoon Dev <doublemmon.dev01@gmail.com>
2021-04-27 20:55:34 +0700

    Change all to doublemoon

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -1,4 +1,4 @@
-import { MenuEntry } from '@pancakeswap-libs/uikit'
+import { MenuEntry } from '@doublemoondev/doublemoon-uikit'
 
 const config: MenuEntry[] = [
   {
--- src/hooks/LanguageContext.ts
+++ src/hooks/LanguageContext.ts
@@ -1,5 +1,5 @@
 import React, { createContext } from 'react'
-import { LangType } from '@pancakeswap-libs/uikit'
+import { LangType } from '@doublemoondev/doublemoon-uikit'
 
 export interface LanguageObject {
   code: string
