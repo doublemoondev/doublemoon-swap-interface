6db1dc9e88390a0f7939b121a415613533fc69fb
DoubleMoon Dev <doublemmon.dev01@gmail.com>
2021-06-29 17:18:09 +0700

    Update menu vaults and charts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -1,4 +1,4 @@
-import { MenuEntry } from '@doublemoondev/doublemoon-uikit'
+import { MenuEntry, menuStatus } from '@doublemoondev/doublemoon-uikit'
 
 const config: MenuEntry[] = [
   {
@@ -22,9 +22,29 @@ const config: MenuEntry[] = [
     href: 'https://app.doublemoon.finance/pools',
   },
   {
+    label: "Moon Vaults",
+    icon: 'VaultIcon',
+    href: '/vaults',
+    status: menuStatus.NEW,
+  },
+  {
     label: "Moon Referral",
     icon: "ReferralIcon",
     href: "https://app.doublemoon.finance/referral",
+  },
+  {
+    label: "Charts",
+    icon: "ChartIcon",
+    items: [
+      {
+        label: "DBM",
+        href: "https://dex.guru/token/0x0314e5a39806c30d67b869ee1bcdabee7e08de74-bsc",
+      },
+      {
+        label: "DMC",
+        href: "https://dex.guru/token/0xa7bd090bc58e663388be63d5e8a7b683f5a3d4fc-bsc",
+      },
+    ],
   },
   {
     label: "Document",
